300c373558b12001b15efb5b397537e585916c4a
succusana <scamantus@gmail.com>
2021-12-26 10:23:14 +0000

    Add kiss and makeout commands

---
+++ commands/kiss.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { clientId } = require('../config.json');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kiss')
+		.setDescription('Kiss someone!')
+		.addUserOption(option => option.setName('target') .setDescription('Who to kiss') .setRequired(true)),
+
+	async execute(interaction) {
+		// Choose random gif to add to reply.
+		const kissFiles = [
+			'https://cdn.discordapp.com/attachments/924603816781959218/924603885568548894/unknown.gif',
+			'https://cdn.discordapp.com/attachments/924603816781959218/924603886126374922/unknown_3.gif',
+			'https://cdn.discordapp.com/attachments/924603816781959218/924603886612910100/unknown_2.gif',
+			'https://cdn.discordapp.com/attachments/924603816781959218/924603886881374268/unknown_1.gif',
+			'https://cdn.discordapp.com/attachments/924603816781959218/924603887380484156/KanD.gif',
+		];
+		const chosenKiss = kissFiles[Math.floor(Math.random() * kissFiles.length)] ;
+		const selfKiss = 'https://cdn.discordapp.com/attachments/924603816781959218/924605389650137098/unknown.gif';
+		// Preparing target and sender for message.
+		const kissTarget = interaction.options.getUser('target') + '';
+		const kissSender = interaction.user.id;
+		if (kissTarget === clientId) {
+			const kissEmbed = new MessageEmbed()
+				.setColor('#FFC0CB')
+				.setDescription('Mmh~?!')
+				.setImage(`${selfKiss}`);
+			await interaction.reply({ embeds: [kissEmbed] });
+		}
+		else {
+			console.log(`Target is ${kissTarget}, not <@923637342219157564>.`);
+			const kissEmbed = new MessageEmbed()
+				.setColor('#8F3BCB')
+				.setDescription(`<@${kissSender}> kisses <@${kissTarget}>!`)
+				.setImage(`${chosenKiss}`);
+
+			await interaction.reply({ embeds: [kissEmbed] });
+		}
+	},
+};
---
+++ commands/makeout.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { clientId } = require('../config.json');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('makeout')
+		.setDescription('Kiss someone with a *lot* more passion.')
+		.addUserOption(option => option.setName('target') .setDescription('Who to make love to') .setRequired(true)),
+
+	async execute(interaction) {
+		// Choose random gif to add to reply.
+		const makeoFiles = [
+			'https://cdn.discordapp.com/attachments/924604377103208510/924605010258575360/tumblr_n8tnsfbJik1rrr564o2_r1_500.gif',
+			'https://cdn.discordapp.com/attachments/924604377103208510/924605010610888794/200.gif',
+			'https://cdn.discordapp.com/attachments/924604377103208510/924606823946919986/unknown_2.gif',
+			'https://cdn.discordapp.com/attachments/924604377103208510/924606824248926228/unknown_1.gif',
+		];
+		const chosenMakeO = makeoFiles[Math.floor(Math.random() * makeoFiles.length)] ;
+		const selfMakeO = 'https://cdn.discordapp.com/attachments/924604377103208510/924607998649851934/unknown_1.gif';
+		// Preparing target and sender for message.
+		const makeoTarget = interaction.options.getUser('target') + '';
+		const makeoSender = interaction.user.id;
+		if (makeoTarget === clientId) {
+			const makeoEmbed = new MessageEmbed()
+				.setColor('#FFC0CB')
+				.setDescription('Wait, you expect me to do *that?*')
+				.setImage(`${selfMakeO}`);
+			await interaction.reply({ embeds: [makeoEmbed] });
+		}
+		else {
+			console.log(`Target is ${makeoTarget}, not <@923637342219157564>.`);
+			const makeoEmbed = new MessageEmbed()
+				.setColor('#8F3BCB')
+				.setDescription(`<@${makeoSender}> makes out with <@${makeoTarget}>...? Lewd!`)
+				.setImage(`${chosenMakeO}`);
+
+			await interaction.reply({ embeds: [makeoEmbed] });
+		}
+	},
+};
