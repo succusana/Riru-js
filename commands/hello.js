784901587033795e219dd99f6154ea14bb8319e5
succusana <scamantus@gmail.com>
2021-12-26 17:43:25 +0000

    Fixing typos.

--- commands/hello.js
+++ commands/hello.js
@@ -7,9 +7,9 @@ module.exports = {
 		.setDescription('Greets the user!'),
 	async execute(interaction) {
 		const helloEmbed = new MessageEmbed()
-			.setcolor('#FFC0CB')
-			.setdescription(`Hihi <@${interaction.user.id}>!`)
-			.setimage('https://cdn.discordapp.com/attachments/924631967771795516/924631985878597663/unknown.gif');
+			.setColor('#FFC0CB')
+			.setDescription(`Hihi <@${interaction.user.id}>!`)
+			.setImage('https://cdn.discordapp.com/attachments/924631967771795516/924631985878597663/unknown.gif');
 		await interaction.reply({ embeds: [helloEmbed] });
 	},
 };
