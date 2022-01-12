0ba3638a2769dde0568af9e91b33e20bcb38fe4c
succusana <scamantus@gmail.com>
2022-01-12 20:16:43 +0000

    Remove reference to Riru's original write.

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Ready! Riru V2 is now online! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Riru is now online! Logged in as ${client.user.tag}`);
 	},
 };
