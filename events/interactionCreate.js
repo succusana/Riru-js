ce60f448f56ba7bdf8820a508dd335d428054f14
succusana <scamantus@gmail.com>
2022-03-06 10:46:44 +0000

    Fixing bug where Riru would crash if command sent in DM.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,6 +2,12 @@ module.exports = {
 	name:'interactionCreate',
 	execute(interaction) {
 		const timeStamp = Date().toLocaleString();
-		console.log(`${timeStamp} - ${interaction.user.tag} triggered ${interaction.commandName}. Sent from ${interaction.channel.name} in ${interaction.guild.name}.`);
+		const isServer = interaction.inGuild();
+		if (isServer == false) {
+			console.log(`${timeStamp} - ${interaction.user.tag} triggered ${interaction.commandName} in DMs.`);
+		}
+		else {
+			console.log(`${timeStamp} - ${interaction.user.tag} triggered ${interaction.commandName}. Sent from ${interaction.channel.name} in ${interaction.guild.name}.`);
+		}
 	},
 };
