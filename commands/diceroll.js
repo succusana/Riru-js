741eec57b279be8ea86526123914f456f98c6d3c
succusana <succusana@gmail.com>
2022-08-07 20:54:27 +0100

    Add indicator of options to Diceroll command.

--- commands/diceroll.js
+++ commands/diceroll.js
@@ -27,7 +27,7 @@ module.exports = {
 
 		const interEmbed = new MessageEmbed()
 			.setColor('#FFC0CB')
-			.setTitle(`${interSender} rolls the dice!`)
+			.setTitle(`${interSender} rolls the dice (from ${interMinVal} to ${interMaxVal}!`)
 			.setDescription(`*And the result is...* \n ***${diceResult}!***`)
 			.setImage(`${usedGif}`);
 		await interaction.reply({ embeds: [interEmbed] });
