e06ca78e345769b543c005cb877002108ceea4b5
succusana <succusana@gmail.com>
2022-11-14 19:10:00 +0000

    Add new interaction to hug command.

--- commands/hug.js
+++ commands/hug.js
@@ -22,9 +22,13 @@ module.exports = {
 			'https://cdn.discordapp.com/attachments/924373854044946433/924373912375148604/hug3.gif',
 			'https://cdn.discordapp.com/attachments/924373854044946433/924373911800533112/hug5.gif',
 			'https://cdn.discordapp.com/attachments/924373854044946433/924373911439814726/hug6.gif',
+			'https://cdn.discordapp.com/attachments/986692191311261707/1041789830729842688/9d9fe47c884b653409906508531bda2130ab0667_hq.gif',
+			'https://cdn.discordapp.com/attachments/986692191311261707/1041791816564998184/tumblr_p9lw4y2VWb1tx8o4yo1_540.gif',
+			'https://cdn.discordapp.com/attachments/986692191311261707/1041791842129281146/tumblr_p76znmBQt51u86t2qo1_500.gif',
 		];
 		const chosenInter = interFiles[Math.floor(Math.random() * interFiles.length)] ;
 		const selfInter = 'https://cdn.discordapp.com/attachments/924373854044946433/924378713632698368/hugself.gif';
+		const loneInter = 'https://cdn.discordapp.com/attachments/924373854044946433/1041789067630747748/enage-kiss-anime-hug.gif'
 		// Preparing target and sender for message.
 		const interTarget = interaction.options.getUser('target') + '';
 		const interSender = interaction.user.id;
@@ -39,6 +43,14 @@ module.exports = {
 				.setTitle('Riru gets hugged!')
 				.setDescription('Wait-!\n*Riru accepts the hug, blushing a fair bit but hugging back gently.*')
 				.setImage(`${selfInter}`);
+			await interaction.reply({ embeds: [interEmbed] });
+		}
+		else if (interTarget === interSender) {
+			const interEmbed = new MessageEmbed()
+				.setColor('#FFC0CB')
+				.setTitle('You get a hug from Riru instead!')
+				.setDescription('If there\'s nobody else to  give you a hug, you can just ask...')
+				.setImage(`${loneInter}`);
 			await interaction.reply({ embeds: [interEmbed] });
 		}
 		else {
